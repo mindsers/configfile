da539c47a6edaa1ba1c870dc3e15de9813809ccd
Mindsers <dev@nathanaelcherrier.com>
2017-10-09 03:18:59 +0200

    Use only FsUtils class

--- src/shared/fs.utils.js
+++ src/shared/fs.utils.js
@@ -2,42 +2,6 @@ const fs = require('fs')
 const util = require('util')
 const path = require('path')
 
-const { log } = require('./log.utils')
-
-function fileExist(filename = '') {
-  try {
-    fs.accessSync(filename)
-  } catch (e) {
-    return false
-  }
-
-  return true
-}
-
-function mkdirp(pathname = '') {
-  const pathParts = pathname.split('/')
-  const mkdir = util.promisify(fs.mkdir)
-
-  return pathParts.reduce((promise, part) => {
-    return promise
-      .then((previousPath) => {
-        const newPath = path.join(previousPath, part)
-
-        if (fileExist(newPath)) {
-          return newPath
-        }
-
-        return mkdir(newPath)
-          .catch(error => {
-            if (error.code !== 'EEXIST') {
-              throw new Error('dir_create_failed')
-            }
-          })
-          .then(() => newPath)
-      })
-  }, Promise.resolve(pathname[0] === '/' ? '/' : ''))
-}
-
 class FsUtils {
   static get chmod() {
     return util.promisify(fs.chmod)
@@ -49,6 +13,14 @@ class FsUtils {
 
   static get symlink() {
     return util.promisify(fs.symlink)
+  }
+
+  static get writeFile() {
+    return util.promisify(fs.writeFile)
+  }
+
+  static get mkdir() {
+    return util.promisify(fs.mkdir)
   }
 
   static fileExist(filename = '') {
@@ -63,7 +35,6 @@ class FsUtils {
 
   static mkdirp(pathname = '') {
     const pathParts = pathname.split('/')
-    const mkdir = util.promisify(fs.mkdir)
 
     return pathParts.reduce((promise, part) => {
       return promise
@@ -74,7 +45,7 @@ class FsUtils {
             return newPath
           }
 
-          return mkdir(newPath)
+          return FsUtils.mkdir(newPath)
             .catch(error => {
               if (error.code !== 'EEXIST') {
                 throw new Error('dir_create_failed')
@@ -86,12 +57,4 @@ class FsUtils {
   }
 }
 
-module.exports = exports = {
-  fileExist,
-  writeFile: util.promisify(fs.writeFile),
-  readFile: util.promisify(fs.readFile),
-  mkdir: util.promisify(fs.mkdir),
-  symlink: util.promisify(fs.symlink),
-  mkdirp,
-  FsUtils
-}
+module.exports = exports = { FsUtils }
