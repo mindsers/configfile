1cf54a80227555d29e9bc72ebbd5f3d2779c5d11
Mindsers <dev@nathanaelcherrier.com>
2017-10-10 07:59:15 +0200

    Create scriptsCommand

--- src/commands.js
+++ src/commands.js
@@ -1,9 +1,11 @@
 const initCommand = require('./init/init.command')
 const runCommand = require('./run/run.command')
 const deployCommand = require('./deploy/deploy.command')
+const scriptsCommand = require('./scripts.commande')
 
 module.exports = exports = {
   initCommand,
   runCommand,
-  deployCommand
+  deployCommand,
+  scriptsCommand
 }
