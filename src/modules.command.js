580eb4cd7e3362e1cb02d98d23d38dce5ff5c582
Mindsers <dev@nathanaelcherrier.com>
2017-10-13 07:27:42 +0200

    Add error message for ConfigurationFileNotExist

--- src/modules.command.js
+++ src/modules.command.js
@@ -1,7 +1,19 @@
 const { LogUtils } = require('./shared/log.utils')
 
+const { ConfigurationFileNotExist } = require('./services/config')
+
 module.exports = exports = fileService => () => {
-  const modules = fileService.modules
+  let modules = []
+
+  try {
+    modules = fileService.modules
+  } catch (e) {
+    if (e instanceof ConfigurationFileNotExist) {
+      LogUtils.log({ message: 'Unable to load modules without knowing where are stored the data.' })
+      LogUtils.log({ type: 'error', message: 'No configuration file. You need to run the init command before.' })
+      return
+    }
+  }
 
   if (modules.length < 1) {
     LogUtils.log({ type: 'info', message: `No module found.` })
