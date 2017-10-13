d4cdfbe80c3c25177d298f303c7336feeb77f3d1
Mindsers <dev@nathanaelcherrier.com>
2017-10-13 07:29:51 +0200

    Scripts: add err msg for ConfigurationFileNotExist

--- src/scripts.command.js
+++ src/scripts.command.js
@@ -1,7 +1,19 @@
 const { LogUtils } = require('./shared/log.utils')
 
+const { ConfigurationFileNotExist } = require('./services/config')
+
 module.exports = exports = fileService => () => {
-  const scripts = fileService.scripts
+  let scripts = []
+
+  try {
+    scripts = fileService.scripts
+  } catch (e) {
+    if (e instanceof ConfigurationFileNotExist) {
+      LogUtils.log({ message: 'Unable to load scripts without knowing where are stored the data.' })
+      LogUtils.log({ type: 'error', message: 'No configuration file. You need to run the init command before.' })
+      return
+    }
+  }
 
   if (scripts.length < 1) {
     LogUtils.log({ type: 'info', message: `No scripts found.` })
