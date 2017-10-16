678c913b02c4355dad100fce4cd981ce0c8b2733
Mindsers <dev@nathanaelcherrier.com>
2017-10-16 15:59:31 +0200

    Add todo for FileService#runScript

--- tests/services/file.service.js
+++ tests/services/file.service.js
@@ -3,3 +3,6 @@ import test from 'ava'
 test.todo('FileService#scripts should return name and path of valid script')
 test.todo('FileService#modules should return name and path of valid modules')
 test.todo('FileService#modules should return target and source of each file in modules')
+test.todo('FileService#runScript should call chmod')
+test.todo('FileService#runScript should call execFile')
+test.todo(`FileService#runScript should rethrows BadScriptPermission when 'EACCESS' error is thrown`)
