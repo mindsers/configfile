e5e80c354170ae85df09c8c3abed5975c6f68062
Mindsers <dev@nathanaelcherrier.com>
2017-10-16 18:04:10 +0200

    Chage todos to tests for config.service

--- tests/services/config.service.js
+++ tests/services/config.service.js
@@ -5,7 +5,7 @@ import rimraf from 'rimraf'
 
 import { ConfigService, ConfigurationFileNotExist } from '../../src/services/config'
 
-const tmpData = path.join(__dirname, './data')
+const tmpData = path.join(__dirname, './env-config')
 let configService
 
 beforeEach('init temp files for tests', t => {
@@ -38,10 +38,48 @@ test('ConfigService#configData should throw an error when main configuration fil
   t.throws(() => configService.configData(), ConfigurationFileNotExist)
 })
 
-test.todo('ConfigService#scriptExtension should return default value when it is not specified in file')
-test.todo('ConfigService#scriptExtension should return existing value')
-test.todo('ConfigService#scriptExtension should check is an array before update')
-test.todo('ConfigService#folderPath should return existing value')
-test.todo('ConfigService#folderPath should update value')
-test.todo('ConfigService#repoUrl should return existing value')
-test.todo('ConfigService#repoUrl should update value')
+test('ConfigService#scriptExtension should return default value when it is not specified in file', t => {
+  fs.writeFileSync(configService.configPath, JSON.stringify({}))
+  t.deepEqual(configService.scriptExtension, ['.js', '.sh', ''])
+})
+
+test('ConfigService#scriptExtension should return existing value', t => {
+  fs.writeFileSync(configService.configPath, JSON.stringify({ 'script_extensions': ['.sh', '.vb'] }))
+  t.deepEqual(configService.scriptExtension, ['.sh', '.vb'])
+})
+
+test('ConfigService#scriptExtension should check is an array before update', t => {
+  fs.writeFileSync(configService.configPath, JSON.stringify({}))
+
+  configService.scriptExtension = 3
+  t.not(configService.scriptExtension, 3)
+
+  configService.scriptExtension = ['.ts']
+  t.deepEqual(configService.scriptExtension, ['.ts'])
+})
+
+test('ConfigService#folderPath should return existing value', t => {
+  fs.writeFileSync(configService.configPath, JSON.stringify({ 'folder_path': './test' }))
+  t.is(configService.folderPath, './test')
+})
+
+test('ConfigService#folderPath should update value', t => {
+  fs.writeFileSync(configService.configPath, JSON.stringify({ 'folder_path': './test' }))
+  configService.folderPath = './coucou'
+
+  const data = JSON.parse(fs.readFileSync(configService.configPath))
+  t.is(data['folder_path'], './coucou')
+})
+
+test('ConfigService#repoUrl should return existing value', t => {
+  fs.writeFileSync(configService.configPath, JSON.stringify({ 'repo_url': 'http://github.com' }))
+  t.is(configService.repoUrl, 'http://github.com')
+})
+
+test('ConfigService#repoUrl should update value', t => {
+  fs.writeFileSync(configService.configPath, JSON.stringify({ 'repo_url': 'http://github.com' }))
+  configService.repoUrl = 'http://google.com'
+
+  const data = JSON.parse(fs.readFileSync(configService.configPath))
+  t.is(data['repo_url'], 'http://google.com')
+})
