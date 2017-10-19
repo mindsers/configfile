894ef78190f8a9b9442080efc146aad9f0edcbd8
Mindsers <dev@nathanaelcherrier.com>
2017-10-20 00:28:29 +0200

    Add test for modules attribute in FileService

--- tests/services/file.service.js
+++ tests/services/file.service.js
@@ -14,8 +14,15 @@ beforeEach('init temp files for tests', t => {
   fs.mkdirSync(tmpData)
   fs.mkdirSync(path.join(tmpData, 'scripts'))
   fs.mkdirSync(path.join(tmpData, 'files'))
+
   fs.writeFileSync(path.join(tmpData, 'scripts/test1.sh'), '')
   fs.writeFileSync(path.join(tmpData, 'scripts/test2.js'), '')
+
+  fs.mkdirSync(path.join(tmpData, 'files/module1'))
+  fs.mkdirSync(path.join(tmpData, 'files/"module 2"'))
+  fs.mkdirSync(path.join(tmpData, 'files/badmodule'))
+  fs.writeFileSync(path.join(tmpData, 'files/"module 2"/settings.json'), '[]')
+  fs.writeFileSync(path.join(tmpData, 'files/module1/settings.json'), '[]')
 })
 
 beforeEach('init service', t => {
@@ -32,12 +39,18 @@ afterEach.always('clean temp files', t => {
 
 test('FileService#scripts should return name and path of valid scripts', t => {
   t.deepEqual(fileService.scripts.map(el => el.script), ['test1'])
+  t.is(fileService.scripts.filter(el => ('path' in el)).length, 1)
 
   configService.scriptExtension = ['.js', '.sh', '.flg']
   t.deepEqual(fileService.scripts.map(el => el.script), ['test1', 'test2'])
+  t.is(fileService.scripts.filter(el => ('path' in el)).length, 2)
 })
 
-test.todo('FileService#modules should return name and path of valid modules')
+test('FileService#modules should return name and path of valid modules', t => {
+  t.deepEqual(fileService.modules.map(el => el.module), ['module-2', 'module1'])
+  t.is(fileService.modules.filter(el => ('path' in el)).length, 2)
+})
+
 test.todo('FileService#modules should return target and source of each file in modules')
 test.todo('FileService#runScript should call chmod')
 test.todo('FileService#runScript should call execFile')
