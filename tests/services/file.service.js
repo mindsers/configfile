0e97ce07b571d32b8a3a507da5778d2df3de83d8
Mindsers <dev@nathanaelcherrier.com>
2017-10-20 00:15:24 +0200

    Add test for scripts attribute of FileService

--- tests/services/file.service.js
+++ tests/services/file.service.js
@@ -1,23 +1,42 @@
 import test, { beforeEach, afterEach } from 'ava'
-import { stub } from 'sinon'
+import fs from 'fs'
+import path from 'path'
+import rimraf from 'rimraf'
 
 import { FileService } from '../../src/services/file'
 
+const tmpData = path.join(__dirname, './env-file')
+
 let fileService
 let configService
+
+beforeEach('init temp files for tests', t => {
+  fs.mkdirSync(tmpData)
+  fs.mkdirSync(path.join(tmpData, 'scripts'))
+  fs.mkdirSync(path.join(tmpData, 'files'))
+  fs.writeFileSync(path.join(tmpData, 'scripts/test1.sh'), '')
+  fs.writeFileSync(path.join(tmpData, 'scripts/test2.js'), '')
+})
+
 beforeEach('init service', t => {
-  configService = stub({
-    get folderPath() { return false },
-    get scriptExtension() { return false }
-  })
+  configService = {
+    folderPath: tmpData,
+    scriptExtension: ['.sh']
+  }
   fileService = new FileService(configService)
 })
 
-afterEach('restore spies', t => {
-  configService.restore()
+afterEach.always('clean temp files', t => {
+  rimraf.sync(tmpData)
 })
 
-test.todo('FileService#scripts should return name and path of valid script')
+test('FileService#scripts should return name and path of valid scripts', t => {
+  t.deepEqual(fileService.scripts.map(el => el.script), ['test1'])
+
+  configService.scriptExtension = ['.js', '.sh', '.flg']
+  t.deepEqual(fileService.scripts.map(el => el.script), ['test1', 'test2'])
+})
+
 test.todo('FileService#modules should return name and path of valid modules')
 test.todo('FileService#modules should return target and source of each file in modules')
 test.todo('FileService#runScript should call chmod')
