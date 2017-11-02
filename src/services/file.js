346fa351b6c0c17c486dad3dbd85f974f48fb4aa
Mindsers <dev@nathanaelcherrier.com>
2017-11-02 12:12:37 +0100

    Declare TargetFileAlreadyExist in service index

--- src/services/file.js
+++ src/services/file.js
@@ -1,9 +1,11 @@
 const { FileService } = require('./file/file.service')
 const { BadScriptPermission } = require('./file/bad-script-permission.error')
 const { ScriptNotExist } = require('./file/script-not-exist.error')
+const { TargetFileAlreadyExist } = require('./file/target-file-already-exist.error')
 
 module.exports = exports = {
   FileService,
   BadScriptPermission,
-  ScriptNotExist
+  ScriptNotExist,
+  TargetFileAlreadyExist
 }
