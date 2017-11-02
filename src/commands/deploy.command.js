9b1dc1b79b14a910e6a5db0b421c84a878907699
Mindsers <dev@nathanaelcherrier.com>
2017-11-02 11:29:01 +0100

    Refoctor deploy command

--- src/commands/deploy.command.js
+++ src/commands/deploy.command.js
@@ -6,9 +6,29 @@ module.exports = exports = fileService => (modules, options) => {
   const localDeployment = options.local || false
 
   Promise.resolve()
-    .then(scriptPath => LogUtils.log({ type: 'info', message: `Deployment (${modules.join(', ')}) start.` }))
-    .then(() => fileService.deployModules(modules, !localDeployment))
-    .then(scriptPath => LogUtils.log({ type: 'success', message: `Deployment task is finished.` }))
+    .then(_ => LogUtils.log({ type: 'info', message: `Deployment (${modules.join(', ')}) start.` }))
+    .then(_ => {
+      const files = fileService.modules
+        .filter(element => modules.includes(element.module))
+        .reduce((files, element) => {
+          for (const file of element.settings) {
+            files.push(file)
+          }
+
+          return files
+        }, [])
+        .filter(element => element.global === !localDeployment)
+
+      // fileService.deployModules(modules, !localDeployment)
+      return Promise.all(files.map(file => {
+        if (file.global) {
+          return fileService.deployGlobalFile(file)
+        }
+
+        return fileService.deployLocalFile(file)
+      }))
+    })
+    .then(_ => LogUtils.log({ type: 'success', message: `Deployment task is finished.` }))
     .catch(error => {
       if (error instanceof ConfigurationFileNotExist) {
         LogUtils.log({ type: 'error', message: 'No configuration file. You need to run the init command before.' })
