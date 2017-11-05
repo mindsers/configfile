181b1c7b67e68fd087afd62677f50a3791dd1dc1
Mindsers <dev@nathanaelcherrier.com>
2017-11-05 23:35:41 +0100

    Add file / remove file

--- src/services/ateration-logger/alteration-logger.service.js
+++ src/services/ateration-logger/alteration-logger.service.js
@@ -14,6 +14,22 @@ class AlterationLoggerService {
       fs.mkdirSync(this.dataPath)
     }
   }
+
+  alterationFilePath(moduleName) {
+    return `${this.dataPath}/${moduleName}.json`
+  }
+
+  resetAlterations(moduleName) {
+    if (!FsUtils.fileExist(this.alterationFilePath(moduleName))) {
+      return
+    }
+
+    fs.unlinkSync(this.alterationFilePath(moduleName))
+  }
+
+  addAlteration(moduleName, path, type) {
+    fs.writeFileSync(this.alterationFilePath(moduleName), JSON.stringify({}, null, 4))
+  }
 }
 
 module.exports = exports = { AlterationLoggerService }
