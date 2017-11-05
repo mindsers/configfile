fb8624beea5cfda64e7b7f5437af7b8d56ee90fa
Mindsers <dev@nathanaelcherrier.com>
2017-11-05 23:15:14 +0100

    Init data folder

--- src/services/ateration-logger/alteration-logger.service.js
+++ src/services/ateration-logger/alteration-logger.service.js
@@ -1,6 +1,18 @@
+const fs = require('fs')
+
+const { FsUtils } = require('../../shared/utils')
+
 class AlterationLoggerService {
+  get dataPath() {
+    return `${this.configService.configFolderPath}/alterations`
+  }
+
   constructor(configService) {
     this.configService = configService
+
+    if (!FsUtils.fileExist(this.dataPath)) {
+      fs.mkdirSync(this.dataPath)
+    }
   }
 }
 
