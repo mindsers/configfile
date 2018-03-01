cf7836f5ff2d9b93e23312876ac4aca83bcdebcd
Mindsers <dev@nathanaelcherrier.com>
2018-03-01 22:03:27 +0100

    Create deploy service

--- src/services/file/index.js
+++ src/services/file/index.js
@@ -1,7 +1,5 @@
 const { FileService } = require('./file.service')
-const { TargetFileAlreadyExist } = require('./target-file-already-exist.error')
 
 module.exports = exports = {
-  FileService,
-  TargetFileAlreadyExist
+  FileService
 }
