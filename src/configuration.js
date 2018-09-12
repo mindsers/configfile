a1cd7b6b1e828ee8a558cd3b442f0ae528f550a0
Mindsers <dev@nathanaelcherrier.com>
2018-09-12 23:08:50 +0200

    Remove alteration service as WIP

--- src/configuration.js
+++ src/configuration.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const { InjectorService, DeployService, ExecService, FileService, ConfigService, EventManagerService, AlterationService } = require('./services')
+const { InjectorService, DeployService, ExecService, FileService, ConfigService, EventManagerService } = require('./services')
 
 const packageData = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json')))
 const optionsFilePath = packageData.config.optionsFilePath.replace('~', process.env.HOME)
@@ -13,9 +13,6 @@ injector.provide(ExecService)
 injector.provide(FileService, [ConfigService])
 injector.provide(ConfigService, [{ useValue: optionsFilePath }])
 injector.provide(EventManagerService)
-injector.provide(AlterationService, [ConfigService, EventManagerService])
-
-injector.get(AlterationService)
 
 module.exports = exports = {
   package: packageData
