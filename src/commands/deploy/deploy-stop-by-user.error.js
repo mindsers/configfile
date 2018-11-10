dded158f81ed2bb1c235c0914ca5b36475f50f04
Mindsers <dev@nathanaelcherrier.com>
2018-11-10 05:56:17 +0100

    Use ES6 import syntax in commands

--- src/commands/deploy/deploy-stop-by-user.error.js
+++ src/commands/deploy/deploy-stop-by-user.error.js
@@ -1,7 +1,5 @@
-class DeployStopedByUser extends Error {
+export class DeployStopedByUser extends Error {
   constructor(message = 'User stop the deployment phase.') {
     super(message)
   }
 }
-
-module.exports = exports = { DeployStopedByUser }
