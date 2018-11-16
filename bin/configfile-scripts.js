ca0c346912493c99e98327c4f66f29cdc6ce5051
Mindsers <dev@nathanaelcherrier.com>
2018-11-16 22:02:40 +0100

    Use MessageService in ListScriptsCommand

--- bin/configfile-scripts.js
+++ bin/configfile-scripts.js
@@ -9,7 +9,8 @@ const {
   ConfigService,
   OPTION_PATH_FILE_TOKEN,
   getOptionsFilePath,
-  getPackageData
+  getPackageData,
+  MessageService
 } = require('../src')
 
 ;(() => {
@@ -20,7 +21,7 @@ const {
   cli.description = 'Custom scripts manager.'
 
   cli.register(RunScriptCommand, [ExecService, FileService])
-  cli.register(ListScriptsCommand, [FileService])
+  cli.register(ListScriptsCommand, [FileService, MessageService])
 
   cli.provide({ identity: OPTION_PATH_FILE_TOKEN, useValue: getOptionsFilePath() })
   cli.provide(FileService, [ConfigService])
