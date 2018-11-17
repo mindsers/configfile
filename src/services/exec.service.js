5b3bc452433bc7d54c4a6bc3ff44c55b9d7c3a87
Mindsers <dev@nathanaelcherrier.com>
2018-11-17 16:53:59 +0100

    Refactor ExecService

--- src/services/exec.service.js
+++ src/services/exec.service.js
@@ -2,10 +2,6 @@ import { FsUtils, ProcessUtils } from '../shared/utils'
 import { ScriptNotExist, BadScriptPermission } from '../shared/errors'
 
 export class ExecService {
-  constructor(messageService) {
-    this.messageService = messageService
-  }
-
   async runScript(script) {
     if (script == null) {
       throw new ScriptNotExist(script.script)
@@ -13,17 +9,8 @@ export class ExecService {
 
     await FsUtils.chmod(script.path, '0700')
 
-    const child = ProcessUtils.execFile(script.path)
-
-    child.stdout.on('data', data => {
-      this.messageService.printRawText(data.trim())
-    })
-    child.stderr.on('data', data => {
-      this.messageService.printRawError(data.trim())
-    })
-
     try {
-      return await child.toPromise()
+      return await ProcessUtils.spawn(script.path)
     } catch (error) {
       if (error.code === 'EACCES') {
         throw new BadScriptPermission(script.script)
