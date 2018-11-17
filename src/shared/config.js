3a4162553ca6ed8712ebae671ce03e490c19fca5
Mindsers <dev@nathanaelcherrier.com>
2018-11-17 15:26:44 +0100

    Set log level to ERROR

--- src/shared/config.js
+++ src/shared/config.js
@@ -5,4 +5,4 @@ import { LOG_LEVEL } from '../services'
 export const OPTION_PATH_FILE_TOKEN = new InjectionToken()
 
 export const APP_LOG_LEVEL_TOKEN = new InjectionToken()
-export const APP_LOG_LEVEL = LOG_LEVEL.ALL
+export const APP_LOG_LEVEL = LOG_LEVEL.ERROR
